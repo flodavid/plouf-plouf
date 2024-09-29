07f535f74513a0efad3b2e0a2a8df0f1ffec7536
flodavid <fl.david.53@gmail.com>
2024-09-29 18:41:57 +0200

    Add test on reinsert value

--- src/components/DrawResult/ActionButtons/ActionButtons.tsx
+++ src/components/DrawResult/ActionButtons/ActionButtons.tsx
@@ -65,6 +65,7 @@ const ActionButtons = ({ slug, hidden }: ActionButtonsProps) => {
         </button>
         {draw.draw.previousValues.length > 0 ? (
           <button
+            data-cy="ActionButtons_reinsertButton"
             className={styles.button}
             type="button"
             onClick={() => reinsertValues(draw.draw) }
